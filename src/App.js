7fac172a5e9f7d19522713f95dde4947557e0207
NoemiJ01 <89852801+NoemiJ01@users.noreply.github.com>
2022-03-28 12:26:25 -0400

    Day 1 input price range

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import RangeInput from "./Components/RangeInput";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {};
   }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello React App</h1>
-      </div>
-    );
+  class App extends Component {
+    render() {
+      return (
+        <div className="container">
+          <ul className="cards">
+            <RangeInput />
+          </ul>
+        </div>
+      );
+    }
   }
-}
 
 export default App;
---
+++ src/Components/RangeInput.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+class RangeInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      range: null
+    };
+  }
+
+  // The event will be passed in automatically.
+  handleChange = (event) => {
+    this.setState({ range: event.target.value });
+  };
+
+  render() {
+    return (
+      <li className="RangeInput cards-item">
+        <div className="card">
+          <div className="card-content">
+            <div className="card-image"></div>
+            <div className="card-title">Range Slider</div>
+            <p className="card-text">
+              <div className="input">
+                <input
+                  type="range"
+                  className="rangeInput"
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="output">
+                <label for="rangeOutput">State: </label>
+                <span name="rangeOutput">{this.state.range}</span>
+              </div>
+            </p>
+          </div>
+        </div>
+      </li>
+    );
+  }
+}
+
+export default RangeInput;
