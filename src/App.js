5cf26139944f0f39da2238a084385a2c4a391fb3
NoemiJ01 <89852801+NoemiJ01@users.noreply.github.com>
2022-04-14 12:25:47 -0400

    API_data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import React, { Component } from "react";
 import "./App.css";
-import Navbar from "./Components/Navbar";
-import RangeInput from "./Components/RangeInput";
-import SelectInput from "./Components/SelectInput";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      data: []
+    };
   }
-
+  componentDidMount = () => {
+    fetch(
+      "https://data.cityofnewyork.us/resource/43nn-pn8j.json?$$app_token=v9CzGzRoHQTpZejn2Bqdz0XPy"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ data: data });
+        console.log(this.state.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
   render() {
     return (
-      <div className="container">
-        <ul className="cards">
-          <Navbar />
-        </ul>
+      <div className="App">
+        <h1>Food Violations</h1>
+        <section className="list">
+          {/* PUT API RESPONSE DATA HERE */}
+          <ul>
+            {this.state.data.map((restaurant) => (
+              <li>{restaurant.dba}</li>
+            ))}
+          </ul>
+        </section>
       </div>
     );
   }
